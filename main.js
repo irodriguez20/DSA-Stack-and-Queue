e55521b535bfce798ef973c7b0177da6c22e7253
India Rodriguez <carnes.s.ia17@gmail.com>
2020-02-15 09:52:36 -0700

    paused at exercise 8

--- main.js
+++ main.js
@@ -13,11 +13,10 @@ class Stack {
     push(data) { // O(1) or constant complexity
         /* If the stack is empty, then the node will be the
         top of the stack*/
-        if (this.top === null) {
-            this.top = new _Node(data, null);
-            return this.top;
-        }
-
+        // if (this.top === null) {
+        //     this.top = new _Node(data, null);
+        //     return this.top;
+        // }
         /* IF the stack already has something, 
         thien create a new node, 
         add data to the new node, and have the pointer point to the top */
@@ -33,15 +32,84 @@ class Stack {
         this.top = node.next;
         return node.data;
     }
+
+    peek() {
+        const node = this.top
+        if (node === null) {
+            return null;
+        }
+        return node.data;
+    }
+
+    isEmpty() {
+        return this.top === null;
+    }
+
+    display() {
+        return this.top;
+    }
 }
 
-function main() {
-    const starTrek = new Stack
-    starTrek.push('Kirk');
-    starTrek.push('Spock');
-    starTrek.push('McCoy');
-    starTrek.push('Scotty');
-    console.log(starTrek)
+const starTrek = new Stack();
+
+starTrek.push('Kirk');
+starTrek.push('Spock');
+starTrek.push('McCoy');
+// starTrek.push('Scotty');
+// console.log(starTrek);
+// console.log(starTrek.peek());
+// console.log(starTrek.isEmpty());
+// starTrek.pop();
+// starTrek.pop();
+// console.log(starTrek);
+
+
+
+function is_palindrome(sentence) {
+    const palindrome = new Stack();
+    let letters = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""); //never want to reasign or push to an array received from parameters.
+
+    // Your code goes here
+    for (let i = 0; i < letters.length; i++) {
+        palindrome.push(letters[i]);
+    }
+
+    let reverseWord = "";
+    while (palindrome.isEmpty() === false) {
+        reverseWord = reverseWord + palindrome.pop()
+    }
+
+    return reverseWord === letters;
 }
 
-console.log(main())
+// True, true, true, false
+// console.log(is_palindrome("dad"));
+// console.log(is_palindrome("A man, a plan, a canal: Panama"));
+// console.log(is_palindrome("1001"));
+// console.log(is_palindrome("Tauhida"));
+
+function sortStack(stackToSort) {
+    const orderedStack = new Stack();
+    orderedStack.push(5);
+
+    while (stackToSort.isEmpty() === false) {
+        if (stackToSort.peek() >= orderedStack.peek()) {
+            const temp = stackToSort.pop();
+            orderedStack.push(temp);
+        } else {
+            if (stackToSort.peek() <= orderedStack.peek()) {
+                const temp = stackToSort.pop();
+                orderedStack.push(temp);
+            }
+        }
+    }
+
+    return orderedStack;
+
+}
+const stackToSort = new Stack();
+stackToSort.push(1);
+stackToSort.push(2);
+stackToSort.push(3);
+stackToSort.push(4);
+console.log(sortStack(stackToSort));
--- queue.js
+++ queue.js
@@ -1,7 +1,8 @@
-class _Node {
+class Node {
     constructor(value) {
         this.value = value;
         this.next = null;
+        this.prev = this.next;
     }
 }
 
@@ -9,10 +10,11 @@ class Queue {
     constructor() {
         this.first = null;
         this.last = null;
+        this.prev = null;
     }
 
     enqueue(data) { // O(1) or constant complexity
-        const node = new _Node(data);
+        const node = new Node(data);
 
         if (this.first === null) {
             this.first = node;
@@ -39,5 +41,38 @@ class Queue {
         return node.value;
     }
 
+    display() {
+        if (this.first === null) {
+            return 'empty';
+        }
+        const node = this;
+        return node;
+    }
+
 }
 
+const starTrekQ = new Queue();
+starTrekQ.enqueue('Kirk');
+starTrekQ.enqueue('Spock');
+starTrekQ.enqueue('Uhura');
+starTrekQ.enqueue('Sulu');
+starTrekQ.enqueue('Checkov');
+starTrekQ.dequeue();
+starTrekQ.dequeue();
+console.log(starTrekQ);
+
+function peek(starTrekQ) {
+    let node = starTrekQ.display();
+    return node.first;
+}
+console.log(peek(starTrekQ))
+
+function isEmpty(queueToCheck) {
+    let node = peek(queueToCheck);
+    if (node === undefined) {
+        return `is Empty`
+    }
+    return node.first;
+}
+const emptyQ = new Queue();
+console.log(isEmpty(emptyQ));
